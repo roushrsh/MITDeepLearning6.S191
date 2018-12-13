da352eace2c1aadc9bc5788b81b6c7b6e15e69ab
Roush <roushrsh@hotmail.com>
2018-12-12 17:45:06 -0800

    Update predictingNumbersFromImageCNN.py

--- Lab2/predictingNumbersFromImageCNN.py
+++ Lab2/predictingNumbersFromImageCNN.py
@@ -31,6 +31,7 @@ x_input = tf.reshape(x, [-1, 28, 28, 1])
 # first convolutional layer
 # 32 5x5 filters with stride of 1, ReLU activation
 conv1 = tf.layers.conv2d(inputs=x_input, filters=32, kernel_size=[5,5], activation=tf.nn.relu)
+conv1 = tf.nn.dropout(conv1, 0.96)
 # first pooling layer
 # 2x2 max pooling with stride of 2
 pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2,2], strides=[2,2])
@@ -40,6 +41,8 @@ pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2,2], strides=[2,2])
 '''TODO: define a convolutional layer with 48 5x5 filters with stride of 1, ReLU activation'''
 
 conv2 = tf.layers.conv2d(inputs = pool1, filters=48, kernel_size =[5,5], activation =tf.nn.relu)
+
+
 
 # second pooling layer
 '''TODO: define a 2x2 max pooling layer with stride of 2'''
@@ -124,10 +127,3 @@ with tf.Session() as sess:
     preds = prediction.eval(feed_dict={x: mnist.test.images})
     conf_mat = tf.contrib.metrics.confusion_matrix(labels=tf.argmax(mnist.test.labels,1), predictions=preds, num_classes=10)
     plt.imshow(conf_mat.eval())
-
-
-
-
-
-
-
